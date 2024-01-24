6d4f55060d8f7e0cb206babc7ea66547ddc85032
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-01-24 07:01:36 -0600

    Beginning server.js setup

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employeeTracker_db'
+    },
+    console.log('Connected to the employeeTracker_db database')
+);
