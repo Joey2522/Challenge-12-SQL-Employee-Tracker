17aefa298c260432b8dc2ea35109b65a9b3ff153
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-01-24 11:02:37 -0600

    Database connection not working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+console.log('hello world');
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
+// const { Connection } = require('mysql2/typings/mysql/lib/Connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,12 +10,48 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = mysql.createConnection (
-    {
+const connection = mysql.createConnection ({
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'employeeTracker_db'
-    },
-    console.log('Connected to the employeeTracker_db database')
-);
+        database: 'employeeTracker_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the employeeTracker_db database!");
+    start();
+});
+
+// function questions() {
+//     inquirer
+//         .prompt ({
+//             type: 'list',
+//             name: 'question1',
+//             message: 'What would you like to do?',
+//             choices: [
+//                 "View all departments.",
+//                 "View all roles.",
+//                 "View all emplyees.",
+//                 "Add a department.",
+//                 "Add a role.",
+//                 "Add an employee.",
+//                 "Update an employee role",
+//                 "Exit",
+//             ],
+
+//         })
+//         .the((answer) => {
+//             switch (answer.action) {
+//                 case"View all departments":
+//                 viewAllDepartments();
+//                 break;
+//             }
+//         })
+//     };
+
+//     start();
+
+app.listen(PORT, () => {
+console.log(`Server runnning on http://localhost:${PORT}`);
+});
