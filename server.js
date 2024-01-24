7c249d8d2d607076f36e242fee4976996d0fae17
Joey2522 <148152563+Joey2522@users.noreply.github.com>
2024-01-24 13:54:36 -0600

    Working database connection

--- server.js
+++ server.js
@@ -20,37 +20,36 @@ const connection = mysql.createConnection ({
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected to the employeeTracker_db database!");
-    start();
 });
 
-// function questions() {
-//     inquirer
-//         .prompt ({
-//             type: 'list',
-//             name: 'question1',
-//             message: 'What would you like to do?',
-//             choices: [
-//                 "View all departments.",
-//                 "View all roles.",
-//                 "View all emplyees.",
-//                 "Add a department.",
-//                 "Add a role.",
-//                 "Add an employee.",
-//                 "Update an employee role",
-//                 "Exit",
-//             ],
+function questions() {
+    inquirer
+        .prompt ({
+            type: 'list',
+            name: 'question1',
+            message: 'What would you like to do?',
+            choices: [
+                "View all departments.",
+                "View all roles.",
+                "View all emplyees.",
+                "Add a department.",
+                "Add a role.",
+                "Add an employee.",
+                "Update an employee role",
+                "Exit",
+            ],
 
-//         })
-//         .the((answer) => {
-//             switch (answer.action) {
-//                 case"View all departments":
-//                 viewAllDepartments();
-//                 break;
-//             }
-//         })
-//     };
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case"View all departments":
+                viewAllDepartments();
+                break;
+            }
+        })
+    };
 
-//     start();
+    questions();
 
 app.listen(PORT, () => {
 console.log(`Server runnning on http://localhost:${PORT}`);
